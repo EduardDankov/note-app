779964d6d900925181a18d73491d756cbda76f51
Eduard Dankov <eduard.dankov@gmail.com>
2024-04-19 01:21:01 +0300

    feat: add database context with note storage
    This adds an in-memory EF Core database context with a storage for the Note model instances.

---
+++ NoteApp.Api/Database.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using NoteApp.Api.Models;
+
+namespace NoteApp.Api;
+
+/// <summary>
+/// Initializes a database context of the application.
+/// Implements <see cref="DbContext"/> class with the specified options.
+/// </summary>
+/// <param name="options">The options for the database context.</param>
+public class Database(DbContextOptions<Database> options) : DbContext(options)
+{
+    /// <summary>
+    /// Storage for the instances of the <see cref="Note"/> entity.
+    /// </summary>
+    public DbSet<Note> Notes => Set<Note>();
+}
--- NoteApp.Api/Program.cs
+++ NoteApp.Api/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+using NoteApp.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Database>(optionsAction => optionsAction.UseInMemoryDatabase("NoteApp"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddControllers().AddControllersAsServices();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
